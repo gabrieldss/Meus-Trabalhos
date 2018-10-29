30c09c02547d106df24305c282c1eb69a55a15d9
gabrieldss <33532210+gabrieldss@users.noreply.github.com>
2018-10-29 18:13:35 -0300

    Add files via upload

---
+++ T2/Meu-Portfolio-React/src/components/Footer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <center>
+      <div className="rodape">
+              <div className="header-menu-items">
+                <div className="menu-item2">
+                <p className="rod">© 2018 Gabriel Souza</p>
+              </div>
+              </div>
+      </div>
+      </center>
+        );
+  }
+}
+
+export default Footer;
